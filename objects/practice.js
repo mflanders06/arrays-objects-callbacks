5a388fff029c66e9bb8066639d6f9c7fa596007e
MFlanders <mflanders06@gmail.com>
2021-01-29 20:08:34 -0700

    Verified main questions done, and some object ext

--- objects/practice.js
+++ objects/practice.js
@@ -124,8 +124,8 @@ let methodCollection = {};
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
-methodCollection.alertHello = function(){ 'hello' };
-methodCollection.logHello = function(){ 'logHello' };
+methodCollection.alertHello = function(){ alert('hello'); };
+methodCollection.logHello = function(){ console.log('hello'); };
 
 //Code Here
 
